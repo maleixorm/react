42400fc014fed59a201952e5d9e77751c6a462d3
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-22 06:35:11 -0300

    Utilizando variáveis em strings

---
+++ variaveis/strings.js
@@ -0,0 +1,7 @@
+let nome = "Marcos";
+let sobrenome = "Marques";
+let idade = 37;
+let pessoa = 'Meu nome é ' + nome + ' ' + sobrenome + ' e eu tenho ' + idade + ' anos de idade.';
+console.log(pessoa)
+let pessoa2 = `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos de idade.`;
+console.log(pessoa2)
