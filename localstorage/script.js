82aa595309cc3b2ec8abe1f4c5a6cb1a0e6b4010
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-21 21:59:29 -0300

    Trabalhando com localStorage e sessionStorage

---
+++ localstorage/script.js
@@ -0,0 +1,8 @@
+let nome = '';
+
+if (typeof localStorage.nome == 'undefined') {
+    localStorage.nome = prompt('Digite seu nome:');
+}
+
+nome = localStorage.nome;
+document.getElementById('nome').innerHTML = nome;
