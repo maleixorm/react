58bda25e2141ada8b53f42efdd0552102a524f6b
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-22 13:31:26 -0300

    Trabalhando com funções anônimas

---
+++ funcoes_anonimas.js
@@ -0,0 +1,9 @@
+function adicionar(...numeros) {
+    let total = numeros.reduce((total, proximo) => {
+        let soma = total + proximo;
+        return soma;
+    });
+    console.log(total);
+}
+
+adicionar(1,2,3,4,5,6,7);
