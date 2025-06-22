c7ca01edcec2287fd1301a686b74654d067c61de
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-22 06:28:45 -0300

    Variaveis e suas características

---
+++ variaveis/variaveis.js
@@ -0,0 +1,19 @@
+var nome = "Marcos";
+
+if (nome == "Marcos") {
+    var idade = 37;
+    console.log(`${nome} tem ${idade} anos.`);
+}
+
+let nome2 = "Matheus";
+
+if (nome2 == "Matheus") {
+    let sobrenome = "Silva";
+    console.log(`${nome2} ${sobrenome} tem ${idade} anos.`);
+}
+
+const trabalho = "Analista de TI";
+
+// let é acessado apenas dentro do escopo
+// var é acessado de forma global(fora do escopo)
+// const é uma variavel que não tem seu valor alterado
